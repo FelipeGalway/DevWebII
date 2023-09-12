67e3cefa800c4cd79745ba35abb8bd9a617d8408
Felipe Ferreira Rezende <felipe_legitimo@hotmail.com>
2023-09-12 10:07:57 -0300

    11/09 - prova

--- Prova/exercicio9.ts
+++ Prova/exercicio9.ts
@@ -1,3 +1,18 @@
 namespace exercicio9 {
-
+    function fibonacci(n: number) {
+        if (n == 1) {
+            return 1;
+        }
+        else if (n == 2) {
+            return 1;
+        }
+        else {
+            let resultado: number = fibonacci(n - 1) + fibonacci(n - 2);
+            return resultado;
+        }
+    }
+    
+    let n = 3;
+    let resultado = fibonacci(n);
+    console.log(`O ${n}-ésimo termo da sequência de Fibonacci é ${resultado}`);
 }
